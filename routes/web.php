74ca446e739a2604665b6e14c8e7cfc74344c073
HydraCommunications <131209560+HydraCommunications@users.noreply.github.com>
2024-10-18 18:43:19 -0500

    removed stupid looking welcome page in favor of forcing login on functional pages

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,8 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/home', function () {
     return Inertia::render('Dashboard');
